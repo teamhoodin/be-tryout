160029e68c436de43fc5a9226cc0cbaef2471fac
Sigrid Storck <sigridstorck@Sigrids-MacBook-Pro.local>
2019-12-17 11:28:46 +0100

    tryout base

---
+++ database/migrations/2019_12_17_000001_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->timestamps();
+            $table->increments('id');
+            $table->string('text');
+            $table->date('rating');
+            $table->integer('user_id');
+            $table->integer('game_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
