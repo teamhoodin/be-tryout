2d3ec3491cd7b43bc02a4d94690cd7fddc8808f4
Simon Almcrantz <simon@Simons-MacBook-Pro.local>
2019-12-19 11:14:25 +0100

    database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Game;
+use App\Models\Review;
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Factory::create();
+
+        User::unguard();
+
+        foreach (range(1, 20) as $index) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->freeEmail,
+                'password' => $faker->md5,
+                'created_at' => $faker->dateTime,
+            ]);
+        }
+
+        foreach (range(1, 20) as $index) {
+            Game::create([
+                'title' => $faker->city . ' ' . $faker->safeColorName,
+                'release_date' => $faker->date,
+                'created_at' => $faker->dateTime,
+            ]);
+        }
+
+        foreach (range(1, 100) as $index) {
+            Review::create([
+                'rating' => $faker->numberBetween(1, 5),
+                'text' => $faker->sentence,
+                'user_id' => User::all()->random()->id,
+                'game_id' => Game::all()->random()->id,
+            ]);
+        }
     }
 }
